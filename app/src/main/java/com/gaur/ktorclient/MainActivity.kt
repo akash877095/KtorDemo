ad99187926a1cfc740b7f9fbe219919ce845f49a
akash877095 <jadhav.akash8770@gmail.com>
2024-05-02 11:52:55 +0530

    Initial Commit

--- app/src/main/java/com/gaur/ktorclient/MainActivity.kt
+++ app/src/main/java/com/gaur/ktorclient/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         CoroutineScope(Dispatchers.IO).launch {
 
             val post = Post(
